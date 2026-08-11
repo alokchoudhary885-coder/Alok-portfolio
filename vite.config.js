f2c63d9225ab4c4d3fe85ecab59f12079193e316
Alok Choudhary <alokchoudhary885@gmail.com>
2026-08-11 10:38:24 +0530

    Fix vite.config.js dynamic base path for localhost dev server and dist build

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: './', // Allow opening dist/index.html directly via Live Server (5500) or static server
+  // Use relative base path for build (dist) and root path for dev server
+  base: command === 'build' ? './' : '/',
   server: {
     port: 3000,
     host: true
   }
-});
+}));
